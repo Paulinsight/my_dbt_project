b3127bad473562901554376c4e075bd70948c02a
Paul SOUCHARD <psouchard@ds-restauration.com>
2025-02-25 09:50:16 +0000

    feat: add _y on column name on stg_sales_database__order_item

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,9 +1,9 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id,
- order_id,
- product_id,
- seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
- price as unit_price,
- shipping_cost,
- (price * quantity) + shipping_cost as total_order_item_amount
+select CONCAT(order_id, '_', product_id) AS order_item_id_y,
+ order_id_y,
+ product_id_y,
+ seller_id_y,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
+ price as unit_price_y,
+ shipping_cost_y,
+ (price * quantity) + shipping_cost as total_order_item_amount_y
 from {{ source('sales_database', 'order_item') }}
